ba027d228f7402f5edf76bb890a35361b5801aa4
Warlyn Piña <88066951+warlynjohan026@users.noreply.github.com>
2025-03-21 18:08:02 -0400

    Fixing presentation

--- bakery-cake-project/src/components/Presentation-Wall/Presentation.jsx
+++ bakery-cake-project/src/components/Presentation-Wall/Presentation.jsx
@@ -7,7 +7,9 @@ function Presentation() {
       <div className={Style.heroTextContainer}>
         <h2>¡Lo hecho en casa sabe mejor!</h2>
         <p>
-         
+          En MaryCake, pongo todo mi amor y dedicación en cada creación. Me
+          encanta cuidar cada detalle para crear piezas únicas, deliciosas y
+          perfectas para tus momentos especiales.
         </p>
         <button>
           <a href="#offer">Saber más</a>
